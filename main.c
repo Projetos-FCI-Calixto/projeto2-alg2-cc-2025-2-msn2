1b04c50d7c0c9e0c9fed639b2f5d6a19fc93a0db
Pedro Botelho <p.botelho2006@gmail.com>
2025-10-31 00:40:36 +0000

    Lógica da função de verificação parcialmente pronta

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+char analise(){
+    //verifica se a imagem recebida é uniforme
+        //se sim, verifica se é P
+            //retorna o P
+        //não sendo P
+            //retorna o B
+
+    //se não é uniforme
+        //retorna X
+        //Calcula os pontos de divisão (esquerda fica com uma coluna a mais e superior fica com uma linha a mais em caso de ímpar)
+        //chama a função de novo e calcula para os 4 quadrantes em ordem (sup. esq; sup. dir; inf. esq; inf. dir)
+
+}
+int main(){
+    return 0;
+}
